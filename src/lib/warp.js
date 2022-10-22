9639d96fe4454d6939265e42886a32e8f04e40e2
Tom Wilson <tom@hyper.io>
2022-10-22 02:43:14 +0000

    fix warp to support stamp contract

--- src/lib/warp.js
+++ src/lib/warp.js
@@ -3,14 +3,19 @@ const { WarpFactory, LoggerFactory } = window.warp
 LoggerFactory.INST.logLevel('error')
 const warp = WarpFactory.forMainnet()
 
-export const readState = (contract) => warp.contract(contract).setEvaluationOptions({
-  internalWrites: true,
-  allowBigInt: true
-}).readState().then(({ cachedValue }) => cachedValue)
+export const readState = (contract) => {
+  console.log({ contract })
+  return warp.contract(contract).setEvaluationOptions({
+    internalWrites: true,
+    allowUnsafeClient: true,
+    allowBigInt: true
+  }).readState().then(({ cachedValue }) => cachedValue)
+}
 
 export const writeInteraction = (contract, input) => warp.contract(contract).connect('use_wallet')
   .setEvaluationOptions({
     internalWrites: true,
+    allowUnsafeClient: true,
     allowBigInt: true
   })
   .writeInteraction(input)
@@ -18,6 +23,7 @@ export const writeInteraction = (contract, input) => warp.contract(contract).con
 export const dryRun = (contract, input) => warp.contract(contract).connect('use_wallet')
   .setEvaluationOptions({
     internalWrites: true,
+    allowUnsafeClient: true,
     allowBigInt: true
   })
   .dryWrite(input)
